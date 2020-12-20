3ceea488ddbc410871048fde03a0ea392304e1e6
Juliano Tavares <julianojstavares@outlook.com>
2020-12-19 22:11:12 -0300

    Update models.py

--- clientes/models.py
+++ clientes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=25)
     email = models.EmailField(blank=False, max_length=30, )
     cpf = models.CharField(max_length=11, unique=True)
     rg = models.CharField(max_length=9)
